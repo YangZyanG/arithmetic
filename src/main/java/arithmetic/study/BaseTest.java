b17aa3c7caf38786ae1fa4b137b57531515b74b8
yangziyang <yangziyang@yohitrip.com>
2020-03-24 15:57:27 +0800

    666

--- src/main/java/arithmetic/study/BaseTest.java
+++ src/main/java/arithmetic/study/BaseTest.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import org.junit.Test;
 
 import java.util.Objects;
-import java.util.Random;
 
 /**
  * @author yangziyang
@@ -100,6 +99,10 @@ public class BaseTest {
 
         private Node next;
 
+        public Node(){
+
+        }
+
         public Node(Object value){
             this.value = value;
         }
@@ -117,7 +120,9 @@ public class BaseTest {
         System.out.println(value);
     }
 
-    //哨兵
+    /***
+     * 哨兵
+     */
     @Test
     public void sentry(){
 
@@ -152,4 +157,184 @@ public class BaseTest {
         long end_2 = System.currentTimeMillis();
         System.out.println(end_2 - start_2);
     }
+
+    /***
+     * 队列
+     */
+    @Data
+    class ArrayQueue{
+
+        private Object[] arrays;
+
+        private int n;
+
+        private int head;
+
+        private int rear;
+
+        private final int capacity = 16;
+
+        public ArrayQueue(){
+            arrays = new Object[capacity];
+            this.n = capacity;
+        }
+
+        public ArrayQueue(int n){
+            arrays = new Object[n];
+            this.n = n;
+        }
+
+        public void push(Object value){
+            if (rear == n){
+                if (head == 0)
+                    return;
+
+                for (int i=head; i<rear; ++i){
+                    arrays[i - head] = arrays[i];
+                    arrays[i] = null;
+                }
+                rear -= head;
+                head = 0;
+            }
+
+            arrays[rear++] = value;
+        }
+
+        public Object pop(){
+            if (rear == head)
+                return null;
+
+            Object value = arrays[head];
+            arrays[head++] = null;
+            return value;
+        }
+    }
+
+    @Test
+    public void ArrayQueueTest(){
+        ArrayQueue queue = new ArrayQueue(10);
+        for (int i=0; i<10; ++i){
+            queue.push(i);
+        }
+        System.out.println(JSONConvert.toString(queue.getArrays()));
+
+        for (int i=0; i<6; ++i){
+            Object value = queue.pop();
+            System.out.println(value);
+        }
+        System.out.println(JSONConvert.toString(queue.getArrays()));
+
+        queue.push(10);
+        System.out.println(JSONConvert.toString(queue.getArrays()));
+    }
+
+    @Data
+    class LinkedQueue{
+
+        private Node head;
+
+        private Node rear;
+
+        public LinkedQueue(){
+
+        }
+
+        public void push(Object value){
+
+            Node last = rear;
+            Node newNode = new Node(value);
+            rear = newNode;
+
+            if (Objects.isNull(head)){
+                head = newNode;
+            }else {
+                last.next = rear;
+            }
+        }
+
+        public Object pop(){
+
+            if (Objects.isNull(head))
+                return null;
+
+            Object value = head.value;
+            head = head.next;
+            return value;
+        }
+    }
+
+    @Test
+    public void linkedQueueTest(){
+        LinkedQueue queue = new LinkedQueue();
+        queue.push("1");
+        queue.push("2");
+        queue.push("3");
+        System.out.println(JSONConvert.toString(queue.getHead()));
+        Object value = queue.pop();
+        System.out.println(value);
+        value = queue.pop();
+        System.out.println(value);
+        System.out.println(JSONConvert.toString(queue.getHead()));
+    }
+
+    /***
+     * 切记：循环队列会浪费数组的一个储存空间
+     */
+    @Data
+    class CycleQueue{
+
+        private Object[] arrays;
+        private int capacity;
+        private int head;
+        private int rear;
+
+        private final int CAP = 16;
+
+        public CycleQueue(){
+            arrays = new Object[CAP];
+            this.capacity = CAP;
+        }
+
+        public CycleQueue(int n){
+            arrays = new Object[n];
+            this.capacity = n;
+        }
+
+        public void push(Object value){
+
+            if ((rear + 1) % capacity == head)
+                return;
+
+            arrays[rear] = value;
+            rear = (rear + 1) % capacity;
+        }
+
+        public Object pop(){
+
+            if (head == rear)
+                return null;
+
+            Object value = arrays[head];
+            arrays[head] = null;
+            head = (head + 1) % capacity;
+            return value;
+        }
+    }
+
+    @Test
+    public void cycleQueueTest(){
+        CycleQueue queue = new CycleQueue();
+        for (int i=0; i<16; ++i){
+            queue.push(i);
+        }
+        System.out.println(JSONConvert.toString(queue.getArrays()));
+
+        Object value = queue.pop();
+        System.out.println(value);
+
+        queue.push(15);
+        System.out.println(JSONConvert.toString(queue.getArrays()));
+    }
+
+    
 }
